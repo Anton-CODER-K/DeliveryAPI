4b50987ba9714b571576c4b333901db57d2efccb
Anton-CODER-K <antonkluckovic@gmail.com>
2026-03-12 18:47:48 +0200

    refactor (Ip)

--- Api/Program.cs
+++ Api/Program.cs
@@ -125,7 +125,15 @@ namespace DeliveryAPI.Api
             builder.Logging.AddConsole();
             builder.Logging.SetMinimumLevel(LogLevel.Debug);
 
+            builder.Services.Configure<ForwardedHeadersOptions>(options =>
+            {
+                options.ForwardedHeaders =
+                    ForwardedHeaders.XForwardedFor |
+                    ForwardedHeaders.XForwardedProto;
 
+                options.KnownNetworks.Clear();
+                options.KnownProxies.Clear();
+            });
 
 
             var app = builder.Build();
