bc1f737aacb170aba512b95eceea1027662e536a
Anton-CODER-K <antonkluckovic@gmail.com>
2026-04-24 16:51:26 +0300

    Add DeliveryCourierResult model and courier delivery query
    Introduce DeliveryCourierResult and related classes for courier-specific delivery data, including user and address details. Update DeliveryService and DeliveryRepository to support new courier delivery queries and conditional data hiding. Refactor controller to accept nullable DeliveryStatus. Move DeliveryUserResult to its own file and clean up unused usings.

---
+++ Application/Models/Result/DeliveryUserResult.cs
@@ -0,0 +1,85 @@
+﻿using DeliveryAPI.Api.Contracts.Response;
+
+namespace DeliveryAPI.Application.Models.Result
+{
+    public class DeliveryUserResult
+    {
+        public int DeliveryId { get; set; }
+
+        public string StatusDelivery { get; set; }
+
+        public string PaymentMethod { get; set; }
+
+        public string? PaymentStatus { get; set; }
+
+        public decimal TotalPrice { get; set; }
+
+        public int Total_weight_grams { get; set; }
+
+        public DateTime CreatedAt { get; set; }
+
+        public List<DeliveryUserItem> Items { get; set; }
+    }
+
+    public class DeliveryUserItem
+    {
+        public string ProductName { get; set; }
+        public int Quantity { get; set; }
+        public decimal TotalLineAmount { get; set; }
+    }
+
+
+
+
+    public class DeliveryCourierResult
+    {
+        public int DeliveryId { get; set; }
+
+        public int RestaurantId { get; set; }
+
+        public int StatusDelivery { get; set; }
+
+        public string PaymentMethod { get; set; }
+
+        public string? PaymentStatus { get; set; }
+
+        public decimal TotalPrice { get; set; }
+
+        public int Total_weight_grams { get; set; }
+
+        public DateTime CreatedAt { get; set; }
+
+        public List<DeliveryCourierItem> Items { get; set; }
+
+        public AddressUserIdByCourierResponse Address { get; set; }
+       
+        public UserResult User {  get; set; }
+
+    }
+
+    public class DeliveryCourierItem
+    {
+        public string ProductName { get; set; }
+        public int Quantity { get; set; }
+        public decimal TotalLineAmount { get; set; }
+    }
+
+    public class UserResult
+    {
+        public string Phone { get; set; }
+        public string Name { get; set; }
+        public string? AvatarUrl { get; set; }
+    }
+
+    public class AddressUserIdByCourierResponse
+    {
+        public decimal latitude { get; set; }
+        public decimal longitude { get; set; }
+        public string house { get; set; }
+        public string? apartment { get; set; }
+        public string? entrance { get; set; }
+        public string? floor { get; set; }
+        public string? comment { get; set; }
+    }
+
+}
